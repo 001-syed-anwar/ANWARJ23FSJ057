ab96970f63f0973b25632710d0e9c628b2a6cce0
Syed Anwar <79622763+anwarrawna463-github@users.noreply.github.com>
2023-07-08 21:54:07 +0530

    Update Reminder.java

--- Assignment/Reminder.java
+++ Assignment/Reminder.java
@@ -4,12 +4,22 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
+ * <b>Assignment from Arithmetic Operations topic</b>
+ * <p>
+ * This class contains a method that implements logic for performing reminder
+ * operation of numbers along with the main method which is already programmed
+ * for taking user inputs and display the result.
  * 
  * @author syed anwar
  * @category Arithmetic operations
  */
 public class Reminder {
-
+	/**
+	 * The main method that is preprogrammed to accept relevant inputs and print the
+	 * output into the console to demonstrate the reminder functionality.
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		/* Try with resource block is being used, so we don't need to explicitly close
 		   the scanner object */
@@ -43,6 +53,7 @@ public class Reminder {
 	}
 
 	/**
+	 * This method does the operation reminder between two integers.
 	 * 
 	 * @param the integer that we want to perform reminder operation
 	 * @param the integer that we want to perform reminder operation with
