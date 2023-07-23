17d8279acc6dbf34ff6b7b30960efbd117403f34
Syed Anwar <79622763+anwarrawna463-github@users.noreply.github.com>
2023-07-23 08:37:53 +0530

    Update CheckPrime.java

--- basic-closed-lab/CheckPrime.java
+++ basic-closed-lab/CheckPrime.java
@@ -15,11 +15,13 @@ public class CheckPrime {
 	}
 
 	public static boolean isPrime(int num) {
+		if(num<=1)
+			return false;
 		//loop till the root of number, to find it's divisors
-		for (int i = 2; i * i <= num; i++) {
+		for (int i = 2; i * i <= num; i++)
 			if (num % i == 0)
 				return false;
-		}
+		
 		return true;
 	}
 }
